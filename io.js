220000c29d33dce5d287b9eb0f49dde270325b17
Weronika Miller <weronika.altowka@gmail.com>
2019-02-27 14:31:05 -0800

    new css changes

--- io.js
+++ io.js
@@ -1,6 +1,7 @@
 const io = require('socket.io')();
 
 io.on('connection', socket => {
+    socket.broadcast.emit('user connected');
     socket.on('new-msg', data => {
         console.log(data);
         io.emit('send-notification', {name: 'notification'});
--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -1,14 +1,31 @@
 const socket = io();
 
+let button = document.getElementById('submitbtn');
+let notification = document.getElementById('notification');
+let bigger = document.getElementsByTagName('i')[0];
+
+
 socket.on('send-notification', data => {
-    console.log(data);
+    makeVisible(notification);
 });
 //instead you will display some message using js!
 //for a few seconds !! 
-let button = document.getElementById('submitbtn');
 
 button.addEventListener('click', function(e) {
     socket.emit('new-msg', {
         message: 'the message has been sent'
     });
 })
+
+
+function makeVisible(el) {
+    el.style.visibility = 'visible';
+    setTimeout(() => {
+        el.style.visibility = 'hidden';
+    }, 4000);
+}
+
+bigger.addEventListener('click', function(evt) {
+    document.getElementsByClassName('content-bottom')[0].firstElementChild.classList.add('full');
+    bigger.style.display = 'none';
+});
